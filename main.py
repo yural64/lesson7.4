df04bcb4b9136e7b7e76f94dbe118263426690e7
yural64 <yural64@gmail.com>
2024-10-23 14:05:51 +0300

    commented code

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 #Модуль для работы со временем
 import time
 
-browser = webdriver.Chrome()
-#Сайт для подключения
-browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
-#Делаем скрин и сохраняем в файл dom.png
-browser.save_screenshot("dom.png")
-#На какое время подключаемся (10 сек)
-time.sleep(5)
-#Открываем другую страницу
-browser.get("https://en.wikipedia.org/wiki/Selenium")
-browser.save_screenshot("selenium.png")
-time.sleep(3)
-#Перезагрузить страницу
-browser.refresh()
-#Отключаемся
-#browser.quit()
+# browser = webdriver.Chrome()
+# #Сайт для подключения
+# browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+# #Делаем скрин и сохраняем в файл dom.png
+# browser.save_screenshot("dom.png")
+# #На какое время подключаемся (10 сек)
+# time.sleep(5)
+# #Открываем другую страницу
+# browser.get("https://en.wikipedia.org/wiki/Selenium")
+# browser.save_screenshot("selenium.png")
+# time.sleep(3)
+# #Перезагрузить страницу
+# browser.refresh()
+# #Отключаемся
+# #browser.quit()
+
