4147c0edea5028d4c5f046cf53ccf67b223c8157
yural64 <yural64@gmail.com>
2024-10-23 13:49:23 +0300

    adding the initial code

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from selenium import webdriver
+#Модуль для работы со временем
+import time
 
+browser = webdriver.Chrome()
+#Сайт для подключения
+browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+#На какое время подключаемся (10 сек)
+time.sleep(10)
+#Отключаемся
+browser.quit()
