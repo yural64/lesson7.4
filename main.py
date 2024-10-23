79e45b157ce1de4331e9f15a69a11e3d01162df1
yural64 <yural64@gmail.com>
2024-10-23 14:01:31 +0300

    add refresh

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
 #Делаем скрин и сохраняем в файл dom.png
 browser.save_screenshot("dom.png")
 #На какое время подключаемся (10 сек)
-time.sleep(10)
+time.sleep(5)
 #Открываем другую страницу
 browser.get("https://en.wikipedia.org/wiki/Selenium")
 browser.save_screenshot("selenium.png")
-time.sleep(10)
+time.sleep(3)
+#Перезагрузить страницу
+browser.refresh()
 #Отключаемся
-browser.quit()
+#browser.quit()
