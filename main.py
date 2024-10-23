8fa15ea7be6c42d7e221cdcb598462d967773d82
yural64 <yural64@gmail.com>
2024-10-23 13:56:22 +0300

    add screenshots

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import time
 browser = webdriver.Chrome()
 #Сайт для подключения
 browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+#Делаем скрин и сохраняем в файл dom.png
+browser.save_screenshot("dom.png")
 #На какое время подключаемся (10 сек)
 time.sleep(10)
 #Открываем другую страницу
 browser.get("https://en.wikipedia.org/wiki/Selenium")
+browser.save_screenshot("selenium.png")
 time.sleep(10)
 #Отключаемся
 browser.quit()
