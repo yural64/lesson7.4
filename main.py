0d430bc6198e187f7ce3ece75666525b1f47a19e
yural64 <yural64@gmail.com>
2024-10-23 13:52:25 +0300

    add page

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ browser = webdriver.Chrome()
 browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
 #На какое время подключаемся (10 сек)
 time.sleep(10)
+#Открываем другую страницу
+browser.get("https://en.wikipedia.org/wiki/Selenium")
+time.sleep(10)
 #Отключаемся
 browser.quit()
